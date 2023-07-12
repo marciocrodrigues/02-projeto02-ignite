2decf8c292a17184021670add76872e7818eef78
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-07-11 22:13:41 -0300

    implantação das funcionalidades do formulario mais validação com react-hook-form, zod e @hookform/resolvers

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import {
   HomeContainer,
@@ -10,16 +13,52 @@ import {
   MinusAmoutInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minuteAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no minimu de 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo de 60 minutos'),
+})
+
+// interface NewCycleFormData {
+//   task: string
+//   minuteAmount: number
+// }
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
+  const { register, handleSubmit, watch, reset, formState } =
+    useForm<NewCycleFormData>({
+      resolver: zodResolver(newCycleFormValidationSchema),
+      defaultValues: {
+        task: '',
+        minuteAmount: 0,
+      },
+    })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(formState.errors)
+    console.log(data)
+    reset()
+  }
+
+  // console.log(formState.errors)
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -28,17 +67,25 @@ export function Home() {
             <option value="Projeto 3" />
           </datalist>
 
-          <label htmlFor="minuteAmout">durante</label>
+          <label htmlFor="minuteAmount">durante</label>
           <MinusAmoutInput
             type="number"
-            id="minuteAmout"
+            id="minuteAmount"
             placeholder="00"
             step={5}
             min={5}
             max={60}
+            {...register('minuteAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
+          {/*
+          Teste para forma de mostrar a mensagem de erro da validação com o zod
+          {formState.errors.minuteAmount ? (
+            <span>{formState.errors.minuteAmount.message}</span>
+          ) : (
+            false
+          )} */}
         </FormContainer>
 
         <CountDownContainer>
@@ -49,7 +96,7 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <StartCountDownButton disabled type="submit">
+        <StartCountDownButton disabled={isSubmitDisabled} type="submit">
           <Play />
           Começar
         </StartCountDownButton>
